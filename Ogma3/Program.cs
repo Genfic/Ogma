1cd02ecdbee1f358423ecd7ad69176da4526d492
Angius <koumarin@gmail.com>
2020-06-18 03:09:19 +0200

    Dockerization! Dockerization!

--- Ogma3/Program.cs
+++ Ogma3/Program.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ogma3.Data;
 
 namespace Ogma3
 {
@@ -16,20 +13,42 @@ namespace Ogma3
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+                try
+                {
+                    var configuration = provider.GetRequiredService<IConfiguration>();
+                    if (configuration.GetValue<bool>("migrateDatabases"))
+                    {
+                        var identityContext = provider.GetRequiredService<ApplicationDbContext>();
+                        await identityContext.Database.MigrateAsync();
+                    }
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            
             await host.InitAsync();
             await host.RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureKestrel(options =>
-                    {
-                        // Opts
-                    })
-                    .UseStartup<Startup>();
+                        {
+                            // Opts
+                        })
+                        .UseStartup<Startup>();
 //                    webBuilder.UseStartup<Startup>();
                 });
+        }
+
     }
 }
