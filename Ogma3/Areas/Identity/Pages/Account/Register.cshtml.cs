32efcb9b441bf4f652b5594be49e14c83e031f9f
Angius <koumarin@gmail.com>
2020-10-24 00:50:01 +0200

    - Modified the registration code to automatically create rating blacklists for new users, based on the default blacklist for anonymous users

--- Ogma3/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ogma3/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -144,6 +144,18 @@ namespace Ogma3.Areas.Identity.Pages.Account
                 inviteCode.UsedDate = DateTime.Now;
                 await _context.SaveChangesAsync();
                 
+                // Setup default blacklists
+                var defaultBlockedRatings = await _context.Ratings
+                    .Where(r => r.BlacklistedByDefault)
+                    .AsNoTracking()
+                    .ToListAsync();
+                var blockedRatings = defaultBlockedRatings.Select(dbr => new BlacklistedRating
+                {
+                    User = user,
+                    Rating = dbr
+                });
+                await _context.BlacklistedRatings.AddRangeAsync(blockedRatings);
+                
                 // Send confirmation code
                 var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                 code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
