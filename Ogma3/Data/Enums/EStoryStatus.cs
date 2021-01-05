fd4f04e2172c943d18b307285210aa4fc0cf8c6d
Angius <koumarin@gmail.com>
2021-01-05 03:07:31 +0100

    - Added a way to change the story completion status

--- Ogma3/Data/Enums/EStoryStatus.cs
+++ Ogma3/Data/Enums/EStoryStatus.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel;
+
 namespace Ogma3.Data.Enums
 {
     public enum EStoryStatus
     {
+        [Description("In Progress")]
         InProgress = 1,
         Completed = 2,
         OnHiatus = 3,
