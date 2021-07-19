d3d94cae40c5afef2a410e4f4157442cd4e34ce6
Angius <koumarin@gmail.com>
2021-07-19 05:03:29 +0200

    IDE autocompletion for this thing is garbage

--- Ogma3/wwwroot/js/src/club-bar.js
+++ Ogma3/wwwroot/js/src/club-bar.js
@@ -1,30 +1,8 @@
 new Vue({
 	el: '#club-bar',
-	data: {
-		route: null,
-		id: null,
-		xcsrf: null,
-		joined: null,
-	},
 	methods: {
-		join: async function () {
-			const {data} = await axios.post(this.route,
-				{ ClubId: this.id }, 
-				{ headers: {"RequestVerificationToken": this.xcsrf} }
-			);
-			this.joined = data;
-		},
 		report: function () {
 			this.$refs.reportModal.visible = true;
 		}
-	},
-	mounted() {        
-		this.route = document.getElementById('data-route').dataset.route;
-		this.id = Number(document.getElementById('data-id').dataset.id);
-		this.joined = document.getElementById('data-joined').dataset.joined.toLowerCase() === 'true';
-		this.xcsrf = document.querySelector('[name=__RequestVerificationToken]').value;
-	},
-	beforeCreate() {
-		document.getElementById('join-btn').classList.remove('join', 'leave');
 	}
 });
