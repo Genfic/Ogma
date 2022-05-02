fcdea08a69bdd316b19f36b911c08d7fda2fe928
Atulin <koumarin@gmail.com>
2022-05-02 05:53:33 +0200

    Added some Typescript OpenAPI generation, mayhaps better than what NSwag Studio can offer

---
+++ Assets/openapi/tsclient/apis/index.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChaptersReadApi';
+export * from './ClubJoinApi';
+export * from './ClubsApi';
+export * from './CommentsApi';
+export * from './CommentsThreadApi';
+export * from './ContentBlockApi';
+export * from './ErrorApi';
+export * from './FaqsApi';
+export * from './FoldersApi';
+export * from './InfractionsApi';
+export * from './InviteCodesApi';
+export * from './NotificationsApi';
+export * from './QuotesApi';
+export * from './RatingsApi';
+export * from './ReportsApi';
+export * from './RolesApi';
+export * from './ShelfStoriesApi';
+export * from './ShelvesApi';
+export * from './SignInApi';
+export * from './SubscriptionsApi';
+export * from './TagsApi';
+export * from './UserActivityApi';
+export * from './UsersApi';
+export * from './VotesApi';
