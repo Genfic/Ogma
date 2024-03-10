b17d97d2065b10251b80e77479330b9c4aaa76b0
Atulin <11233299+Atulin@users.noreply.github.com>
2024-03-11 00:46:47 +0100

    Experimenting with WCs - Maybe ESBuild will be able to build webcomponents after all...? - Removed Qodana btw

--- Ogma3/esbuild.mjs
+++ Ogma3/esbuild.mjs
@@ -1,16 +1,32 @@
 import * as esbuild from "esbuild";
+import { minifyTemplates, writeFiles } from "esbuild-minify-templates";
 
 const start = process.hrtime.bigint();
 
-const ctx = await esbuild.context({
-	entryPoints: ["./wwwroot/js/src/**/*.ts", "./wwwroot/js/src/**/*.js"],
+const baseSettings = {
 	minify: true,
 	bundle: true,
 	sourcemap: true,
-	outdir: "./wwwroot/js/dist",
 	color: true,
 	logLevel: "info"
+};
+
+const bundleContext = await esbuild.context({
+	...baseSettings,
+	entryPoints: ["./wwwroot/js/src/**/*.ts", "./wwwroot/js/src/**/*.js"],
+	outdir: "./wwwroot/js/dist"
 });
+
+const litContext = await esbuild.context({
+	...baseSettings,
+	entryPoints: ["./wwwroot/js/src-webcomponents/**/*.ts"],
+	plugins: [minifyTemplates(), writeFiles()],
+	outdir: "./wwwroot/js/bundle",
+	write: false
+})
+
+// perform build
+const ctx = process.argv.includes('--wc') ? litContext : bundleContext;
 
 if (process.argv.includes("--watch")) {
 	await ctx.watch();
@@ -19,4 +35,5 @@ if (process.argv.includes("--watch")) {
 	await ctx.rebuild();
 	await ctx.dispose();
 	console.log(`Build completed in ${(process.hrtime.bigint() - start) / BigInt(1_000_000)}ms`);
+	process.exit();
 }
