Genfic/Ogma

commit c1efdd8ddbdbd477d3704782a9939f0e0b8c2401

Updated FAQs to use Immediate.Apis

Ogma3/Api/V1/Faqs/CreateFaq.cs
@@ -1,0 +1,54 @@
+using FluentValidation;
+using Immediate.Apis.Shared;
+using Immediate.Handlers.Shared;
+using Markdig;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Ogma3.Data;
+using Ogma3.Data.Faqs;
+using Ogma3.Infrastructure.Constants;
+using Ogma3.Infrastructure.ServiceRegistrations;
+
+namespace Ogma3.Api.V1.Faqs;
+
+using ReturnType = CreatedAtRoute<FaqDto>;
+
+[Handler]
+[MapPost("api/faqs")]
+[Authorize(Policy = AuthorizationPolicies.RequireAdminRole)]
+public static partial class CreateFaq
+{
+	public sealed record Command(string Question, string Answer);
+
+	public class CommandValidator : AbstractValidator<Command>
+	{
+		public CommandValidator()
+		{
+			RuleFor(f => f.Question).NotEmpty();
+			RuleFor(f => f.Answer).NotEmpty();
+		}
+	}
+
+	private static async ValueTask<ReturnType> HandleAsync(
+		Command request,
+		ApplicationDbContext context,
+		CancellationToken cancellationToken
+	)
+	{
+		var (question, answer) = request;
+
+		var faq = new Faq
+		{
+			Question = question,
+			Answer = answer,
+			AnswerRendered = Markdown.ToHtml(answer, MarkdownPipelines.All),
+		};
+		context.Faqs.Add(faq);
+
+		await context.SaveChangesAsync(cancellationToken);
+
+		var dto = new FaqDto(faq.Question, faq.Answer, faq.AnswerRendered);
+
+		return TypedResults.CreatedAtRoute(dto, nameof(GetSingleFaq), new { faq.Id });
+	}
+}

Ogma3/Areas/Admin/Pages/Faq.cshtml
@@ -1,5 +1,4 @@
 @page
-@using Ogma3.Api.V1.Faqs
 @model Faq
 
 @{
@@ -43,11 +42,10 @@
 		<div v-html="f.answerRendered"></div>
 	</details>
 
-	<div v-if="!route" id="route" data-route="@Url.RouteUrl(nameof(FaqsController))"></div>
 	@Html.AntiForgeryToken()
 </div>
 
 @section Scripts
 {
-	<script src="~/js/dist/admin/faqs.js" asp-append-version="true"></script>
+	<script defer src="~/js/dist/admin/faqs.js" asp-append-version="true"></script>
 }

Ogma3/Api/V1/Faqs/Commands/CreateFaq.cs
@@ -1,52 +1,0 @@
-using FluentValidation;
-using Markdig;
-using Mediator;
-using Microsoft.AspNetCore.Mvc;
-using Ogma3.Data;
-using Ogma3.Data.Faqs;
-using Ogma3.Infrastructure.Constants;
-using Ogma3.Infrastructure.Mediator.Bases;
-
-namespace Ogma3.Api.V1.Faqs.Commands;
-
-public static class CreateFaq
-{
-	public sealed record Command(string Question, string Answer) : IRequest<ActionResult<FaqDto>>;
-
-	public class CommandValidator : AbstractValidator<Command>
-	{
-		public CommandValidator()
-		{
-			RuleFor(f => f.Question).NotEmpty();
-			RuleFor(f => f.Answer).NotEmpty();
-		}
-	}
-
-	public class Handler : BaseHandler, IRequestHandler<Command, ActionResult<FaqDto>>
-	{
-		private readonly ApplicationDbContext _context;
-		public Handler(ApplicationDbContext context) => _context = context;
-
-		public async ValueTask<ActionResult<FaqDto>> Handle(Command request, CancellationToken cancellationToken)
-		{
-			var (question, answer) = request;
-
-			var faq = new Faq
-			{
-				Question = question,
-				Answer = answer,
-				AnswerRendered = Markdown.ToHtml(answer, MarkdownPipelines.All)
-			};
-			_context.Faqs.Add(faq);
-
-			await _context.SaveChangesAsync(cancellationToken);
-
-			return CreatedAtAction(
-				nameof(FaqsController.GetFaq),
-				nameof(FaqsController)[..^10],
-				new { faq.Id },
-				new FaqDto(faq.Question, faq.Answer, faq.AnswerRendered)
-			);
-		}
-	}
-}

Ogma3/Api/V1/Faqs/Commands/DeleteFaq.cs
@@ -1,24 +1,0 @@
-using Mediator;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Ogma3.Data;
-using Ogma3.Infrastructure.Mediator.Bases;
-
-namespace Ogma3.Api.V1.Faqs.Commands;
-
-public static class DeleteFaq
-{
-	public sealed record Command(long Id) : IRequest<ActionResult<long>>;
-
-	public class Handler(ApplicationDbContext context) : BaseHandler, IRequestHandler<Command, ActionResult<long>>
-	{
-		public async ValueTask<ActionResult<long>> Handle(Command request, CancellationToken cancellationToken)
-		{
-			var res = await context.Faqs
-				.Where(f => f.Id == request.Id)
-				.ExecuteDeleteAsync(cancellationToken);
-
-			return res > 0 ? Ok(request.Id) : NotFound();
-		}
-	}
-}

Ogma3/Api/V1/Faqs/Commands/UpdateFaq.cs
@@ -1,46 +1,0 @@
-using FluentValidation;
-using Markdig;
-using Mediator;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Ogma3.Data;
-using Ogma3.Infrastructure.Constants;
-using Ogma3.Infrastructure.Mediator.Bases;
-
-namespace Ogma3.Api.V1.Faqs.Commands;
-
-public static class UpdateFaq
-{
-	public sealed record Command(long Id, string Question, string Answer) : IRequest<ActionResult>;
-
-	public class CommandValidator : AbstractValidator<Command>
-	{
-		public CommandValidator()
-		{
-			RuleFor(f => f.Question).NotEmpty();
-			RuleFor(f => f.Answer).NotEmpty();
-		}
-	}
-
-	public class Handler(ApplicationDbContext context) : BaseHandler, IRequestHandler<Command, ActionResult>
-	{
-		public async ValueTask<ActionResult> Handle(Command request, CancellationToken cancellationToken)
-		{
-			var (id, question, answer) = request;
-
-			var rendered = Markdown.ToHtml(answer, MarkdownPipelines.All);
-
-			var res = await context.Faqs
-				.Where(f => f.Id == id)
-				.ExecuteUpdateAsync(f => f
-						.SetProperty(x => x.Question, question)
-						.SetProperty(x => x.Answer, answer)
-						.SetProperty(x => x.AnswerRendered, rendered),
-					cancellationToken);
-
-			await context.SaveChangesAsync(cancellationToken);
-
-			return res > 0 ? Ok() : NotFound();
-		}
-	}
-}

Ogma3/Api/V1/Faqs/DeleteFaq.cs
@@ -1,0 +1,32 @@
+using Immediate.Apis.Shared;
+using Immediate.Handlers.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Ogma3.Data;
+using Ogma3.Infrastructure.ServiceRegistrations;
+
+namespace Ogma3.Api.V1.Faqs;
+
+using ReturnType = Results<NotFound, Ok<long>>;
+
+[Handler]
+[MapDelete("api/faqs")]
+[Authorize(Policy = AuthorizationPolicies.RequireAdminRole)]
+public static partial class DeleteFaq
+{
+	public sealed record Command(long Id);
+
+	private static async ValueTask<ReturnType> HandleAsync(
+		Command request,
+		ApplicationDbContext context,
+		CancellationToken cancellationToken
+	)
+	{
+		var res = await context.Faqs
+			.Where(f => f.Id == request.Id)
+			.ExecuteDeleteAsync(cancellationToken);
+
+		return res > 0 ? TypedResults.Ok(request.Id) : TypedResults.NotFound();
+	}
+}

Ogma3/Api/V1/Faqs/FaqsController.cs
@@ -1,43 +1,0 @@
-﻿using Mediator;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Ogma3.Api.V1.Faqs.Commands;
-using Ogma3.Api.V1.Faqs.Queries;
-using Ogma3.Data.Faqs;
-using Ogma3.Infrastructure.Constants;
-
-namespace Ogma3.Api.V1.Faqs;
-
-[Route("api/[controller]", Name = nameof(FaqsController))]
-[ApiController]
-public class FaqsController(IMediator mediator) : ControllerBase
-{
-	// GET: api/Faqs
-	[HttpGet]
-	public async Task<ActionResult<List<FaqDto>>> GetFaqs()
-		=> await mediator.Send(new GetAllFaqs.Query());
-
-	[HttpGet("{id:long}")]
-	public async Task<ActionResult<FaqDto>> GetFaq(long id)
-		=> await mediator.Send(new GetSingleFaq.Query(id));
-
-	// PUT: api/Faqs/5
-	[HttpPut]
-	[Authorize(Roles = RoleNames.Admin)]
-	[ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status404NotFound)]
-	public async Task<ActionResult> PutFaq(UpdateFaq.Command data)
-		=> await mediator.Send(data);
-
-	// POST: api/Faqs
-	[HttpPost]
-	[Authorize(Roles = RoleNames.Admin)]
-	public async Task<ActionResult<FaqDto>> PostFaq(CreateFaq.Command data)
-		=> await mediator.Send(data);
-
-	// DELETE: api/Faqs/5
-	[HttpDelete("{id:long}")]
-	[Authorize(Roles = RoleNames.Admin)]
-	[ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status404NotFound)]
-	public async Task<ActionResult<long>> DeleteFaq(long id)
-		=> await mediator.Send(new DeleteFaq.Command(id));
-}

Ogma3/Api/V1/Faqs/GetAllFaqs.cs
@@ -1,0 +1,30 @@
+using Immediate.Apis.Shared;
+using Immediate.Handlers.Shared;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Ogma3.Data;
+using Ogma3.Data.Faqs;
+
+namespace Ogma3.Api.V1.Faqs;
+
+using ReturnType = Ok<FaqDto[]>;
+
+[Handler]
+[MapGet("api/faqs")]
+public static partial class GetAllFaqs
+{
+	public sealed record Query;
+	
+	private static async ValueTask<ReturnType> HandleAsync(
+		Query _,
+		ApplicationDbContext context,
+		CancellationToken cancellationToken
+	)
+	{
+		var faqs = await context.Faqs
+			.ProjectToDto()
+			.ToArrayAsync(cancellationToken);
+
+		return TypedResults.Ok(faqs);
+	}
+}

commit 28632f467b91c1eab5473a679240381f48e6dd91

Fixed some errors caused by c1efdd8ddbdbd477d3704782a9939f0e0b8c2401

Ogma3/Api/V1/Faqs/CreateFaq.cs
@@ -35,20 +35,16 @@ public static partial class CreateFaq
 		CancellationToken cancellationToken
 	)
 	{
-		var (question, answer) = request;
-
 		var faq = new Faq
 		{
-			Question = question,
-			Answer = answer,
-			AnswerRendered = Markdown.ToHtml(answer, MarkdownPipelines.All),
+			Question = request.Question,
+			Answer = request.Answer,
+			AnswerRendered = Markdown.ToHtml(request.Answer, MarkdownPipelines.All),
 		};
 		context.Faqs.Add(faq);
 
 		await context.SaveChangesAsync(cancellationToken);
 
-		var dto = new FaqDto(faq.Question, faq.Answer, faq.AnswerRendered);
-
-		return TypedResults.CreatedAtRoute(dto, nameof(GetSingleFaq), new { faq.Id });
+		return TypedResults.CreatedAtRoute(faq.ToDto(), nameof(GetSingleFaq), new GetSingleFaq.Query(faq.Id));
 	}
 }

Ogma3/Areas/Admin/Pages/Faq.cshtml
@@ -21,7 +21,7 @@
 		</div>
 
 		<div class="o-form-group">
-			<input class="o-form-control" type="submit" value="Submit">
+			<input class="o-form-control btn" type="submit" value="Submit">
 		</div>
 
 		<input type="hidden" name="id" v-model="form.id">

Ogma3/Data/Faqs/Faq.cs
@@ -14,4 +14,5 @@ public class Faq : BaseModel
 public static partial class FaqMapper
 {
 	public static partial IQueryable<FaqDto> ProjectToDto(this IQueryable<Faq> q);
+	public static partial FaqDto ToDto(this Faq faq);
 }

Ogma3/Data/Faqs/FaqDto.cs
@@ -1,3 +1,3 @@
 namespace Ogma3.Data.Faqs;
 
-public record FaqDto(string Question, string Answer, string AnswerRendered);
+public record FaqDto(long Id, string Question, string Answer, string AnswerRendered);