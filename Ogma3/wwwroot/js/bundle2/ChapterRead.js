6aefda5481006bd70f454740eee15703abe27264
Atulin <11233299+Atulin@users.noreply.github.com>
2025-04-07 13:41:14 +0200

    Svelte is too much of a PITA to write in JB IDEs

--- Ogma3/wwwroot/js/bundle2/ChapterRead.js
+++
@@ -1,4 +0,0 @@
-import{v as g}from"./Icon.js";import{A as l,B as d,H as e,N as y,R as f,aa as s,ja as c}from"./chunk-hmv3z8hd.js";var E=y('<button class="read-status" aria-label="Chapter read status"><!></button>');function A(m,x){var i=E(),z=d(i);g(z,{icon:"lucide:eye-off"}),l(i),e(()=>s(i,"data-id",x.chapterId)),f(m,i)}var O=A;if(customElements.get("my-element")==null)customElements.define("my-element",c(A,{route:{},chapterId:{}},[],[],!0));export{O as default};
-
-//# debugId=219AD79D30FED7B964756E2164756E21
-//# sourceMappingURL=ChapterRead.js.map
--- Ogma3/wwwroot/js/bundle2/Icon.js
+++
@@ -1,5 +0,0 @@
-import{A as e,B as d,H as l,O as y,R as a,aa as f,ja as m}from"./chunk-hmv3z8hd.js";var E=y('<svg width="24" height="24" class="o-icon svelte-30ugophiyzroy" part="icon"><use></use></svg>');function k(n,x){var i=E(),A=d(i);e(i),l(()=>f(A,"href",`/svg/spritesheet.svg#${x.icon??""}`)),a(n,i)}var F=k;if(customElements.get("my-element")==null)customElements.define("my-element",m(k,{icon:{}},[],[],!0));export{F as default};
-export{F as v};
-
-//# debugId=4FBA514884CB6A5A64756E2164756E21
-//# sourceMappingURL=Icon.js.map
