33f8283e042665fde72a61e59bd3bbeeb9da40ff
Atulin <11233299+Atulin@users.noreply.github.com>
2026-04-15 21:03:14 +0200

    Nuggie uppies for .NET 11-preview3 - Also yeeted the only thing referencing Newtonsoft: the SendGrid mailer 🎉 - Explicitly set AppHost to target .NET 10, since something in 11-preview3 breaks the dashboard

--- Ogma3/Services/Mailer/SendGridOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using JetBrains.Annotations;
-
-namespace Ogma3.Services.Mailer;
-
-public sealed class SendGridOptions
-{
-	public required string SendGridUser { get; [UsedImplicitly] init; }
-	public required string SendGridKey { get; [UsedImplicitly] init; }
-}
