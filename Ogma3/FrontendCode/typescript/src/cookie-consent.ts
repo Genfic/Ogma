c7417393bf5346380058b762d38d36cdbc47b0ab
Atulin <11233299+Atulin@users.noreply.github.com>
2026-05-29 05:57:35 +0200

    Better external login handling - Login/register buttons abstracted to a partial, so changing them has to be done in one place only - External logins in the user panel now use good and proper icons, instead of loading them from external website - Cookie consent cookie is now lax, not strict, so it will persist through new external OAuth connections

--- Ogma3/FrontendCode/typescript/src/cookie-consent.ts
+++ Ogma3/FrontendCode/typescript/src/cookie-consent.ts
@@ -8,6 +8,6 @@ const button = $query<HTMLButtonElement>("button#cookie-consent-button");
 button.addEventListener("click", () => {
 	const expiry = addToDate(new Date(), { years: 100 });
 
-	setCookie("cookie-consent", "true", { expires: expiry, secure: true, sameSite: "Strict", path: "/" });
+	setCookie("cookie-consent", "true", { expires: expiry, secure: true, sameSite: "Lax", path: "/" });
 	banner.style.display = "none";
 });
