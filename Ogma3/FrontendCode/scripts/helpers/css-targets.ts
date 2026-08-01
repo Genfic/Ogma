5758fc7a51aa31833b7a0c9b598bf424272aad91
Atulin <11233299+Atulin@users.noreply.github.com>
2026-08-02 00:33:17 +0200

    Slightly better CSS - body height should better scale on mobile thanks to `svh` - CSS should be guaranteed to support a slightly bigger percentage of browsers now

--- Ogma3/FrontendCode/scripts/helpers/css-targets.ts
+++ Ogma3/FrontendCode/scripts/helpers/css-targets.ts
@@ -1,4 +1,6 @@
 import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
 
-export const cssTargets = browserslistToTargets(browserslist("last 2 years and > 0.1% and not dead"));
+export const cssTargets = browserslistToTargets(
+	browserslist("last 2 years and > 0.1% and last 3 versions and not dead"),
+);
