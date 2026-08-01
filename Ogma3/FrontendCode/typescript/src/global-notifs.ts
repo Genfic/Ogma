9950e6875ce231fa0832d4a2772e27c064f748f4
Atulin <11233299+Atulin@users.noreply.github.com>
2026-08-01 05:51:41 +0200

    10-year expiry on global notifs dismiss cookie

--- Ogma3/FrontendCode/typescript/src/global-notifs.ts
+++ Ogma3/FrontendCode/typescript/src/global-notifs.ts
@@ -28,7 +28,7 @@ for (const notif of notifs) {
 			name: COOKIE_NAME,
 			value: [...dismissed].join(","),
 			sameSite: "lax",
-			expires: Date.now() + 60 * 60 * 60 * 24 * 100,
+			expires: Date.now() + 60 * 60 * 60 * 24 * 365 * 10,
 			path: "/",
 		});
 		notif.remove();
